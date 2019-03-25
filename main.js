f32ba43701964168f1eaaf81a5c7f3370e981659
Jay Yang (jayya) <jayya@cisco.com>
2019-03-25 16:13:29 -0500

    update

--- main.js
+++ main.js
@@ -3,15 +3,43 @@ var dz;
 
 // load dataset and create table
 function load_dataset(csv) {
-  var data = d3.csv.parse(csv)
-  create_table(data);
+    var data = d3.csv.parseRows(csv)
+    var expensesAvgAmount = d3.nest()
+        .key(function(d) { return d[0]; })
+        .rollup(function(v) { return d3.mean(v, function(d) { return d[3]; }); })
+        .entries(data);
+    console.log(JSON.stringify(expensesAvgAmount))  
+    expensesAvgAmount.forEach(function(d){ d.key = new Date(d.key * 1000);});
+    expensesAvgAmount.forEach(function(d){ d.key = d.key.getUTCFullYear()+"-"+('0'+(d.key.getUTCMonth()+1)).slice(-2)+"-"+('0'+(d.key.getUTCDate())).slice(-2)+" "+('0'+(d.key.getUTCHours())).slice(-2)+":"+('0'+(d.key.getUTCMinutes())).slice(-2)+":"+('0'+(d.key.getUTCSeconds())).slice(-2);});   
+    expensesAvgAmount.shift()
+    expensesAvgAmount.pop()
+    create_table(expensesAvgAmount);
+    var trace1 = [{
+        x: unpack(expensesAvgAmount, 'key'),
+        y: unpack(expensesAvgAmount, 'values'),
+        mode: 'lines+markers', 
+        marker: {
+            color: '#98002e',
+            size: 6,
+        },
+        type: 'scatter'
+
+    }];
+    
+    Plotly.newPlot('myDiv', trace1);
 }
+
+
+function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
 
 // create a table with column headers, types, and data
 function create_table(data) {
   // table stats
   var keys = d3.keys(data[0]);
-
+  console.log(data[0])
   var stats = d3.select("#stats")
     .html("")
 
@@ -20,15 +48,6 @@ function create_table(data) {
 
   stats.append("div")
     .text("Rows: " + data.length)
-
-  d3.select("#table")
-    .html("")
-    .append("tr")
-    .attr("class","fixed")
-    .selectAll("th")
-    .data(keys)
-    .enter().append("th")
-      .text(function(d) { return d; });
 
   d3.select("#table")
     .selectAll("tr.row")
@@ -54,7 +73,6 @@ function upload_button(el, callback) {
   uploader.addEventListener("change", handleFiles, false);  
 
   function handleFiles() {
-    d3.select("#table").text("loading...");
     var file = this.files[0];
     reader.readAsText(file);
   };
